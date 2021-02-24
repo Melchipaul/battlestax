3bc73837dac055d06704bae5096db3bafc140f33
Melchipaul <ndumelchipaul2000@gmail.com>
2021-02-24 19:13:02 +0000

    Env

--- .env.sh
+++ .env.sh
@@ -1,5 +1,5 @@
-export ASTRA_DB_ID=d10fa5f3-8780-41c8-80ea-43bb63718dfa
-export ASTRA_DB_REGION=europe-west1
-export ASTRA_DB_USERNAME=battle_user
-export ASTRA_DB_KEYSPACE=battlestax
-export ASTRA_DB_PASSWORD=<database_password>
+ASTRA_DB_ID=d10fa5f3-8780-41c8-80ea-43bb63718dfa
+ASTRA_DB_REGION=europe-west1
+ASTRA_DB_USERNAME=battle_user
+ASTRA_DB_KEYSPACE=battlestax
+ASTRA_DB_PASSWORD=battle_password1
